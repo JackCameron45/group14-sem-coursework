ab1d4ef7b295d625a0d053466d271364638cbd08
JackCameron45 <40436303@live.napier.ac.uk>
2021-02-03 11:39:49 +0000

    Adding docker file and main program class

---
+++ src/main/java/com/napier/semgroup14/MainProgram.java
@@ -0,0 +1,9 @@
+package com.napier.semgroup14;
+
+public class MainProgram
+    {
+        public static void main(String[] args)
+        {
+            System.out.println("Test");
+        }
+    }
