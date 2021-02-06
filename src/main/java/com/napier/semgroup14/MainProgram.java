e34c91aab7196eca4a0fd9f16c7b9773a764730f
SzymonNapier <40508018@live.napier.ac.uk>
2021-02-06 21:50:30 +0000

    comment added

--- src/main/java/com/napier/semgroup14/MainProgram.java
+++ src/main/java/com/napier/semgroup14/MainProgram.java
@@ -2,6 +2,7 @@ package com.napier.semgroup14;
 
 public class MainProgram
     {
+        //github test
         public static void main(String[] args)
         {
             System.out.println("Test");
