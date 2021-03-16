bc7701973a81d8bc4ab98ab7cc4c973c984d99c2
MichalBoduch <40508692@live.napier.ac.uk>
2021-03-15 22:41:38 -0700

    3 tests added

---
+++ src/test/java/TestWithoutRunApp.java
@@ -0,0 +1,63 @@
+import com.napier.semgroup14.City;
+import com.napier.semgroup14.CityAndCountry;
+import com.napier.semgroup14.Country;
+import com.napier.semgroup14.MainProgram;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestWithoutRunApp {
+
+    static  MainProgram mp;
+
+    @BeforeAll
+    static public void   initT() {
+        mp = new MainProgram();
+        mp.connect();
+    }
+    @AfterAll
+    public static      void  deInit() {
+        mp.disconnect();
+    }
+
+    @Test
+    public void query6GetListTest(){
+        ArrayList<Country> countries = mp.query6GetList();
+        System.out.println("Test proba " + countries.size());
+        assertEquals(5, countries.size());
+    }
+
+    @Test
+    public void query7GetListTest(){
+        ArrayList<City> cities = mp.query7GetList();
+        System.out.println("Test proba " + cities.size());
+        assertEquals(4079, cities.size());
+        assertEquals("Seoul", cities.get(1).Name, "Pierwszy element na liście ma się zgadzać");
+    }
+
+    @Test
+    public void query33GetListTest() {
+        ArrayList<CityAndCountry> cityAndCountries = mp.query33GetList();
+//        new BigDecimal( ).setScale(2, RoundingMode.HALF_UP)
+
+        assertEquals(new BigDecimal("32.38"),  roundBg(cityAndCountries.get(0).LanguagePercentage));
+        // skrot
+    }
+
+    public static BigDecimal roundBg(float arg) {
+        return new  BigDecimal(arg).setScale(2, RoundingMode.HALF_UP);
+    }
+
+    @Test
+    public void query6GetList(){
+//        System.out.println("Test proba");
+//        ArrayList<Country> countries = new ArrayList<Country>();
+//        assertEquals(5, countries.size());
+    }
+}
