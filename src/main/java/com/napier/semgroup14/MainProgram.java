9fbc54920bfe0d6aa566d97cc9b9ba4ce83321cc
Ryan Connor <40437041@live.napier.ac.uk>
2021-03-17 13:23:43 +0000

    Updating to new mainProgram version

--- src/main/java/com/napier/semgroup14/MainProgram.java
+++ src/main/java/com/napier/semgroup14/MainProgram.java
@@ -3,7 +3,7 @@ package com.napier.semgroup14;
 import java.sql.*;
 import java.util.ArrayList;
 
-// Class to connect to the MSQL world database
+// Class to connect to the MySQL world database
 public class MainProgram {
     /**
      * Connection to MySQL world database.
@@ -13,29 +13,39 @@ public class MainProgram {
     /**
      * Connect to the MySQL world database.
      */
-    public void connect() {
-        try {
+    public void connect(String location)
+    {
+        try
+        {
             // Load Database driver
-            Class.forName("com.mysql.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }
+        catch (ClassNotFoundException e)
+        {
             System.out.println("Could not load SQL driver");
             System.exit(-1);
         }
 
         int retries = 10;
-        for (int i = 0; i < retries; ++i) {
+        for (int i = 0; i < retries; ++i)
+        {
             System.out.println("Connecting to database...");
-            try {
+            try
+            {
                 // Wait a bit for db to start
                 Thread.sleep(5000);
                 // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "pass");
+                con = DriverManager.getConnection("jdbc:mysql://" + location + "/world?allowPublicKeyRetrieval=true&useSSL=false", "root", "pass");
                 System.out.println("Successfully connected");
                 break;
-            } catch (SQLException sqle) {
+            }
+            catch (SQLException sqle)
+            {
                 System.out.println("Failed to connect to database attempt " + Integer.toString(i));
                 System.out.println(sqle.getMessage());
-            } catch (InterruptedException ie) {
+            }
+            catch (InterruptedException ie)
+            {
                 System.out.println("Thread interrupted? Should not happen.");
             }
         }
@@ -61,7 +71,7 @@ public class MainProgram {
         MainProgram a = new MainProgram();
 
         // Connect to database
-        a.connect();
+        a.connect("db:3306");
 
         // Extract info for query #1
         System.out.println("Query 1, population of countries in world" + "\n");
