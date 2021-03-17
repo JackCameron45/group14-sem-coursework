bb933a6a28e88db94ebb9ffa05a47a32370ad375
MichalBoduch <40508692@live.napier.ac.uk>
2021-03-16 19:20:00 -0700

    Revert "3 tests added #6"
    This reverts commit 0a5934ac

--- src/test/java/TestWithoutRunApp.java
+++ src/test/java/TestWithoutRunApp.java
@@ -6,64 +6,55 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-//@Disabled
+@Disabled
 public class TestWithoutRunApp {
 
-    static MainProgram mp;
+    static  MainProgram mp;
 
-    @Disabled
     @BeforeAll
-    static public void initDatabase() throws ClassNotFoundException, SQLException {
-//        mp = new MainProgram();
-//        mp.connect();
-        System.out.println("Working Directory = " + System.getProperty("user.dir"));
-        Class.forName("org.h2.Driver");
-        Connection conn = DriverManager.getConnection("jdbc:h2:mem:test;INIT=RUNSCRIPT FROM './db/world_db/worldTest.sql';MODE=MYSQL", "sa", "");
+    static public void   initT() {
         mp = new MainProgram();
-        mp.setConnectForTest(conn);
+        mp.connect();
     }
-
-    @Disabled
     @AfterAll
-    public static void deInit() {
+    public static      void  deInit() {
         mp.disconnect();
     }
 
     @Disabled
     @Test
-    public void query6GetListTest() {
+    public void query6GetListTest(){
         ArrayList<Country> countries = mp.query6GetList();
-        System.out.println("Test try " + countries.size());
+        System.out.println("Test proba " + countries.size());
         assertEquals(5, countries.size());
     }
-
-    //  @Disabled
     @Disabled
     @Test
-    public void query7GetListTest() {
+    public void query7GetListTest(){
         ArrayList<City> cities = mp.query7GetList();
         System.out.println("Test proba " + cities.size());
         assertEquals(4079, cities.size());
-        assertEquals("Seoul", cities.get(1).Name, "1st element of the list should match");
+        assertEquals("Seoul", cities.get(1).Name, "Pierwszy element na liście ma się zgadzać");
+    }
+    @Disabled
+    @Test
+    public void query33GetListTest() {
+        ArrayList<CityAndCountry> cityAndCountries = mp.query33GetList();
+//        new BigDecimal( ).setScale(2, RoundingMode.HALF_UP)
+
+        assertEquals(new BigDecimal("32.38"),  roundBg(cityAndCountries.get(0).LanguagePercentage));
+        // skrot
     }
 
- //   @Test
- //   public void query33GetListTest() {
- //       ArrayList<CityAndCountry> cityAndCountries = mp.query33GetList();
- //       assertEquals("32.38", roundBg(cityAndCountries.get(0).LanguagePercentage));
- //   }
-
     public static BigDecimal roundBg(float arg) {
-        return new BigDecimal(arg).setScale(2, RoundingMode.HALF_UP);
+        return new  BigDecimal(arg).setScale(2, RoundingMode.HALF_UP);
     }
 
 }
